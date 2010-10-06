770dd6edf4bb0d78bb3d4aeeda28c3836669b432
karen chan <cee.wing@gmail.com>
2010-10-06 22:22:20 +0100

    Add man page for svn_merge

--- generate_readme.sh
+++ generate_readme.sh
@@ -2,4 +2,7 @@
 
 # This is a script to generate README for github.com
 
+export MANWIDTH=80
 man -l svn_rebase.1 >README
+echo -e "\n\n------------------------------------------------------------\n\n" >>README
+man -l svn_merge.1 >>README
